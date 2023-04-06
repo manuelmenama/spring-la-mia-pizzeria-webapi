cc9cb2b69b10d99dade22d739d09f276fca08ab5
manuelmenama <manuel.mena090589@gmail.com>
2023-04-06 17:59:53 +0200

    started create ingredient form

---
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngredientController.java
@@ -0,0 +1,29 @@
+package org.lessons.springlamiapizzeriacrud.controller;
+
+import jakarta.validation.Valid;
+import org.lessons.springlamiapizzeriacrud.model.Ingredient;
+import org.lessons.springlamiapizzeriacrud.service.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ingredients")
+public class IngredientController {
+    @Autowired
+    IngredientService ingredientService;
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("allIngredient", ingredientService.findAllIngredients());
+        model.addAttribute("newIngredient", new Ingredient());
+        return "/ingredients/index";
+    }
+
+    @PostMapping("/create")
+    public String create(@Valid @ModelAttribute(name="newIngredient") Ingredient ingredient)
+}
